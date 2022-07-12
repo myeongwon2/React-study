604240da4475964976b4ed40b6f7c17ee71b2672
Myeon Won <dnfroge@gmail.com>
2022-07-12 17:29:49 +0900

    List, Key Study

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,12 +11,14 @@ import NotificationList from "./chapter_06/NotificationList";
 import Accommodate from "./chapter_07/Accommodate";
 import ConfirmButton from "./chapter_08/ConfirmButton";
 import LandingPage from "./chapter_09/LandingPage";
+import AttendanceBook from "./chapter_10/AttendanceBook";
 
-ReactDOM.render(
+const rootNode = document.getElementById("root");
+
+ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <AttendanceBook />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
