4612e74a95bddc00464be204809aa3aafefca046
Myeon Won <dnfroge@gmail.com>
2022-07-13 03:11:20 +0900

    Styling Study

--- src/index.js
+++ src/index.js
@@ -16,12 +16,13 @@ import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
 import ProfileCard from "./chapter_13/ProfileCard";
 import DarkOrLight from "./chapter_14/DarkOrLight";
+import Block from "./chapter_15/Blocks";
 
 const rootNode = document.getElementById("root");
 
 ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
-    <DarkOrLight />
+    <Block />
   </React.StrictMode>
 );
 
