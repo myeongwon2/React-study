bb9d82c76c0d257db95bac95d1a05d0657176074
Myeon Won <dnfroge@gmail.com>
2022-07-12 18:48:29 +0900

    Composition, Inheritance Study

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ import LandingPage from "./chapter_09/LandingPage";
 import AttendanceBook from "./chapter_10/AttendanceBook";
 import SignUp from "./chapter_11/SignUp";
 import Calculator from "./chapter_12/Calculator";
+import ProfileCard from "./chapter_13/ProfileCard";
 
 const rootNode = document.getElementById("root");
 
 ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
-    <Calculator />
+    <ProfileCard />
   </React.StrictMode>
 );
 
